72213c64a71d435df443e89040a1ffe418c34f51
Mikhail Shulga <mmshulga@gmail.com>
2018-11-18 22:34:06 +0300

    Completely refactored model entities.

--- src/main/java/my/mmshulga/sfgrecipeproject/bootstrap/RecipeBootstrap.java
+++ src/main/java/my/mmshulga/sfgrecipeproject/bootstrap/RecipeBootstrap.java
@@ -1,6 +1,5 @@
 package my.mmshulga.sfgrecipeproject.bootstrap;
 
-import my.mmshulga.sfgrecipeproject.model.Recipe;
 import my.mmshulga.sfgrecipeproject.repositories.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,8 +7,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class RecipeBootstrap implements CommandLineRunner {
-    private static final int NUMBER_TO_CREATE = 10;
-
     private RecipeRepository recipeRepository;
 
     @Autowired
@@ -19,11 +16,6 @@ public class RecipeBootstrap implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        for (int i = 0; i < NUMBER_TO_CREATE; i++) {
-            Recipe recipe = new Recipe();
-            recipe.setTitle("Wonderful recipe #" + i);
-            recipe.setContent("Add something to recipe #" + i);
-            recipeRepository.save(recipe);
-        }
+
     }
 }
--- src/main/java/my/mmshulga/sfgrecipeproject/model/Recipe.java
+++ src/main/java/my/mmshulga/sfgrecipeproject/model/Recipe.java
@@ -1,18 +1,32 @@
 package my.mmshulga.sfgrecipeproject.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
-    private String content;
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+
+    @Lob
+    private Byte[] image;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     public Long getId() {
         return id;
@@ -22,19 +36,83 @@ public class Recipe {
         this.id = id;
     }
 
-    public String getTitle() {
-        return title;
+    public String getDescription() {
+        return description;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    public String getContent() {
-        return content;
+    public Integer getPrepTime() {
+        return prepTime;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setPrepTime(Integer prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public Integer getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(Integer cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public Integer getServings() {
+        return servings;
+    }
+
+    public void setServings(Integer servings) {
+        this.servings = servings;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+
+    public Notes getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Notes notes) {
+        this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
